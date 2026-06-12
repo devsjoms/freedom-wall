b5ba96956ce5cf64ddca31c900e91ce933696bb0
jomsGutHib <jomarnaval023@gmail.com>
2026-06-13 00:09:14 +0800

    fix: database

--- database/db.js
+++ database/db.js
@@ -1,5 +1,22 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2");
+require("dotenv").config();
+const db = mysql.createPool({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
-const db = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
+db.getConnection((err, connection) => {
+    if(err){
+        console.error(err);
+    } else {
+        console.log("Database connected!");
+        connection.release();
+    }
+});
 
-module.exports = db;
+module.exports = db.promise();
