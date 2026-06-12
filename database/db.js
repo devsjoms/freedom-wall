76caed47d31340226cf01e58114ed53789e5acd9
jomsGutHib <jomarnaval023@gmail.com>
2026-06-12 23:50:28 +0800

    prod prob solved

--- database/db.js
+++ database/db.js
@@ -1,13 +1,7 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 const db = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT,
-    waitForConnections: true,
-    connectionLimit: 10
+    uri: process.env.MYSQL_PUBLIC_URL
 });
 
-module.exports = db.promise();
+module.exports = db;
